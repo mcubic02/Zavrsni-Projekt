cdd69e83875cbf21a692b7ea64d3ef2d0a2a21d5
mcubic02 <mcubic02@fesb.hr>
2023-04-26 22:10:20 +0200

    Prvi commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>Heeeeeeej</div>
+    </>
+  )
+}
+
+export default App
