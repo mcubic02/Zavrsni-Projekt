628cb9c3dc1b80c0de64946f81b4555a00718dec
mcubic02 <mcubic02@fesb.hr>
2023-04-27 12:35:29 +0200

    implementiran react router izbornik

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import Donacije from './components/Donacije'
+import Obavijesti from './components/Obavijesti'
+import OpciPodaci from './components/OpciPodaci'
+import Popis from './components/Popis'
+import Unos from './components/Unos'
 
 
 function App() {
@@ -7,7 +13,32 @@ function App() {
 
   return (
     <>
-    <div>Heeeeeeej</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/" >Opci podaci</Link>
+        </li>
+        <li>
+        <Link to="/obavijesti" >Obavijesti</Link>
+        </li>
+        <li>
+        <Link to="/donacije" >Donacije</Link>
+        </li>
+        <li>
+        <Link to="/popis" >Popis</Link>
+        </li>
+        <li>
+        <Link to="/unos" >Unos</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path = "/" element = {<OpciPodaci />}></Route>
+      <Route path = "/donacije" element = {<Donacije />}></Route>
+      <Route path = "/popis" element = {<Popis />}></Route>
+      <Route path = "/unos" element = {<Unos />}></Route>
+      <Route path = "/obavijesti" element = {<Obavijesti />}></Route>
+    </Routes>
     </>
   )
 }
